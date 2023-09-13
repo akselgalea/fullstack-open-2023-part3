c59907ba2034c85f9941b6a5f7edaca83d30c2e2
Aksel Galea <akselgalea@gmail.com>
2023-09-13 14:18:31 -0300

    Part 3 - Exercise 3.1

--- app.js
+++ app.js
@@ -3,73 +3,34 @@ const app = express()
 app.disable('x-powered-by')
 
 const persons = [
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 1
+  { 
+    id: 1,
+    name: "Arto Hellas", 
+    number: "040-123456"
   },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 2
+  { 
+    id: 2,
+    name: "Ada Lovelace", 
+    number: "39-44-5323523"
   },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 3
+  { 
+    id: 3,
+    name: "Dan Abramov", 
+    number: "12-43-234345"
+  },
+  { 
+    id: 4,
+    name: "Mary Poppendieck", 
+    number: "39-23-6423122"
   }
 ]
 
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
-
-app.use(express.json())
-
 app.get('/', (request, response) => {
-  response.send('Hola gente')
+  response.send('Phonebook API')
 })
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if(person) return response.json(person)
-  
-  response.status(404).end()
-})
-
-app.post('/api/persons', (request, response) => {
-  const { name, number } = request.body
-
-  if(!name || !number) return response.status(400).json({
-    error: 'name and number are required'
-  })
-
-  const person = {
-    id: generateId(),
-    name,
-    number
-  }
-
-  persons.concat(person)
-
-  response.status(201).json(person)
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const filteredPersons = persons.filter(person => person.id !== id)
-  
-  response.status(204).end()
 })
 
 const PORT = 3001
